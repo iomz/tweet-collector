06005555eb3f54aa14ec3a3f491397c6a5f12f41
Iori Mizutani <iori.mizutani@gmail.com>
2021-08-09 17:00:50 +0200

    refactor: fix the code with pylint

--- collector.py
+++ collector.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-from datetime import datetime, timedelta, timezone, tzinfo
-from dateutil.parser import parse
-from natsort import natsorted
-from urllib.parse import parse_qs
 import configparser
 import csv
 import logging
 import os
 import sys
 import time
-import twitter
 
+from datetime import datetime, timedelta, timezone, tzinfo
+from dateutil.parser import parse
+from natsort import natsorted
+
+import twitter
 
 CONFIG_FILE = 'collector.conf'
 DEFAULT_QUERY = 'q=hello%%20&locale=ja&result_type=recent&count=100'
@@ -20,9 +20,10 @@ DEFAULT_DATA_FILE = 'data.csv'
 DEFAULT_DATA_DIR = 'data'
 DEFAULT_SLEEP_INTERVAL = 5
 
+# pylint: disable=R0902
+class Collector():
 
-class Collector(object):
-
+    # pylint: disable=R0913
     def __init__(self,
             consumer_key=None,
             consumer_secret=None,
@@ -48,32 +49,20 @@ class Collector(object):
         self._since_id = most_recent_id
         self._current_month = current_month
         self._rotate_pending = False
+        self._current_result = None
 
         # stdout the config
         logging.info('hatc is initialized')
-        logging.info('base_query: ' + self._base_query)
+        logging.info('base_query: %s', self._base_query)
 
         # initial GetSearch and CheckRateLimit
         # this should get tweets since most_recent_id
-        self._GetSearch()
+        self._get_search()
 
 
-    def _CheckRateLimit(self):
-        while True:
-            try:
-                self._endpoint_rate_limit = self._api.rate_limit.get_limit('/search/tweets')
-            except Exception:
-                logging.debug(e)
-                time.sleep(self._sleep_interval)
-                continue
-            self._remaining_count = int(self._endpoint_rate_limit.remaining)
-            logging.debug(self._endpoint_rate_limit)
-            break
-
-
-    def _GetSearch(self):
+    def _get_search(self):
         while self._endpoint_rate_limit is None:
-            self._CheckRateLimit()
+            self.check_rate_limit()
 
         query = ''
         # 1. there is no history; get everything since as old as possible
@@ -91,13 +80,13 @@ class Collector(object):
             if self._remaining_count == 0:
                 while int(self._endpoint_rate_limit.reset) == 0:
                     logging.debug('retrieving the current rate limit...')
-                    self._CheckRateLimit()
+                    self.check_rate_limit()
                     time.sleep(self._sleep_interval)
                 if self._remaining_count != 0:
                     continue
                 pause_duration = int(self._endpoint_rate_limit.reset) - int(time.time()) + 1
                 if pause_duration > 0:
-                    logging.debug('wait {} seconds before making the request'.format(pause_duration))
+                    logging.debug('wait %d seconds before making the request', pause_duration)
                     time.sleep(pause_duration)
 
             # subtract 1 from remaining_count
@@ -108,11 +97,12 @@ class Collector(object):
                             include_entities=False,
                             return_json=True)
             # coudn't get tweets: something is wrong
-            except Exception as e:
-                logging.warning(e)
+            # pylint: disable=W0703
+            except Exception as exception:
+                logging.warning(exception)
                 time.sleep(self._sleep_interval)
                 continue
-            logging.debug('query: ' + query)
+            logging.debug('query: %s', query)
             break
 
         # set latest_id if None and there are any tweets
@@ -121,38 +111,42 @@ class Collector(object):
             try:
                 self._latest_id = self._current_result['statuses'][0]['id']
                 # something is really wrong
-            except Exception as e:
-                logging.warning(e)
+            # pylint: disable=W0703
+            except Exception as exception:
+                logging.warning(exception)
                 sys.exit(2)
 
 
-    def _WriteToCSV(self, tweet):
+    def _write_to_csv(self, tweet):
         if tweet['text'].startswith('RT @'):
             return
-        with open(self._data_file, 'a') as f:
-            w = csv.writer(f)
-            time = utc_to_jst(parse(tweet['time']))
+        with open(self._data_file, 'a') as data_file:
+            csv_writer = csv.writer(data_file)
+            posted_time = utc_to_jst(parse(tweet['time']))
             text = tweet['text'].replace('\n',' ')
             row = None
             if tweet['coord'] is not None and tweet['coord']['type'] == 'Point':
-                row = [time,tweet['id'],
+                row = [
+                    posted_time,
+                    tweet['id'],
                     tweet['user'],
                     text,
                     tweet['geo_enabled'],
                     tweet['coord']['coordinates'][0],
                     tweet['coord']['coordinates'][1]]
             else:
-                row = [time,
+                row = [
+                    posted_time,
                     tweet['id'],
                     tweet['user'],
                     text,
                     tweet['geo_enabled'],
                     None,
                     None]
-            w.writerow(row)
+            csv_writer.writerow(row)
 
 
-    def _RotateCSV(self):
+    def _rotate_csv(self):
         '''
         Do the following:
         - dump the data from last month to the file
@@ -171,12 +165,12 @@ class Collector(object):
         last_month_data = set()
         new_data = []
         next_month = get_current_month()
-        with open(self._data_file, 'r') as f:
-            for l in f.readlines():
-                if l.startswith(self._current_month):
-                    last_month_data.add(l)
-                elif l.startswith(next_month):
-                    new_data.append(l)
+        with open(self._data_file, 'r') as data_file:
+            for line in data_file.readlines():
+                if line.startswith(self._current_month):
+                    last_month_data.add(line)
+                elif line.startswith(next_month):
+                    new_data.append(line)
 
         # Dump the data from last month to the file
         logging.info('cleaning up the data from the last month...')
@@ -189,12 +183,12 @@ class Collector(object):
             # Backup the file
             os.rename(last_month_data_file, last_month_data_file + ".bak")
 
-        with open(last_month_data_file, 'a') as f:
-            for l in natsorted(last_month_data):
+        with open(last_month_data_file, 'a') as lmdf:
+            for line in natsorted(last_month_data):
                 count += 1
-                f.write(l)
+                lmdf.write(line)
                 if count in percents:
-                    logging.debug('{} % saved'.format(percents[count]))
+                    logging.debug('%d %% saved', percents[count])
 
         # Backup the old data file
         logging.info('performing the data backup')
@@ -202,17 +196,31 @@ class Collector(object):
 
         # Write out the data for the current_month
         logging.info('updating the data')
-        with open(self._data_file, 'a') as f:
-            for l in new_data:
-                f.write(l)
+        with open(self._data_file, 'a') as data_file:
+            for line in new_data:
+                data_file.write(line)
 
         # Update the current_month
-        logging.info('rotated the data file {} -> {}'.format(self._current_month, next_month))
+        logging.info('rotated the data file %s -> %s', self._current_month, next_month)
         logging.info('updating the current_month')
         self._current_month = next_month
 
 
-    def RunForever(self):
+    def check_rate_limit(self):
+        while True:
+            try:
+                self._endpoint_rate_limit = self._api.rate_limit.get_limit('/search/tweets')
+            # pylint: disable=W0703
+            except Exception as exception:
+                logging.debug(exception)
+                time.sleep(self._sleep_interval)
+                continue
+            self._remaining_count = int(self._endpoint_rate_limit.remaining)
+            logging.debug(self._endpoint_rate_limit)
+            break
+
+
+    def run_forever(self):
         while True:
             # there are no tweets in the current iteration; sleep or rotate
             if len(self._current_result['statuses']) == 0:
@@ -221,7 +229,7 @@ class Collector(object):
                     # check the ratation before sleeping
                     if self._rotate_pending:
                         logging.info('month changed; rotating the data')
-                        self._RotateCSV()
+                        self._rotate_csv()
                         self._rotate_pending = False
 
                     # sleep for 12 hours
@@ -230,24 +238,27 @@ class Collector(object):
 
                     # check the current month
                     if get_current_month() != self._current_month:
-                        # if the month changed, the next cycle should cover the end of the previous month
+                        # if the month changed, the next cycle should cover
+                        # the end of the previous month
                         self._rotate_pending = True
 
-                    self._GetSearch()
+                    self._get_search()
                     continue
 
                 logging.info('reached the oldest tweets available; rotating the target range')
                 # set since_id to the latest id we know
                 self._since_id = self._latest_id
-                logging.info('new since_id: {}'.format(self._since_id))
+                logging.info('new since_id: %d', self._since_id)
                 # max_id should be cleared
                 self._max_id = None
                 # latest_id should be cleared
                 self._latest_id = None
                 # proceed to the next cycle
-                logging.debug('wait {} seconds before fetching the next result'.format(self._sleep_interval))
+                logging.debug(
+                        'wait %d seconds before fetching the next result',
+                        self._sleep_interval)
                 time.sleep(self._sleep_interval)
-                self._GetSearch()
+                self._get_search()
                 continue
 
             # from here on process the current iteration
@@ -255,16 +266,23 @@ class Collector(object):
                 # this is the oldest tweet's id in the current iteration
                 self._max_id = self._current_result['statuses'][-1]['id'] - 1
                 # stdout the current max_id
-                logging.debug('new max_id: {}'.format(self._max_id))
+                logging.debug('new max_id: %d', self._max_id)
             # something is really wrong
-            except Exception as e:
-                logging.warning(e)
+            # pylint: disable=W0703
+            except Exception as exception:
+                logging.warning(exception)
                 sys.exit(2)
 
             # save the current result
-            for st in self._current_result['statuses']:
-                tweet = {'time': st['created_at'], 'id': st['id_str'], 'user': st['user']['screen_name'], 'text': st['text'], 'coord': st['coordinates'], 'geo_enabled': st['user']['geo_enabled']}
-                self._WriteToCSV(tweet)
+            for status in self._current_result['statuses']:
+                tweet = {
+                        'time': status['created_at'],
+                        'id': status['id_str'],
+                        'user': status['user']['screen_name'],
+                        'text': status['text'],
+                        'coord': status['coordinates'],
+                        'geo_enabled': status['user']['geo_enabled']}
+                self._write_to_csv(tweet)
                 ## check the current date from the tweets
                 ## this doesn't mean we can alreayd rotate the data...
                 #if st['created_at'][:7] != self._current_month:
@@ -272,12 +290,12 @@ class Collector(object):
 
             # check the number of tweets in this iteration
             number_of_tweets_in_this_cycle = len(self._current_result['statuses'])
-            logging.debug('{} tweets collected'.format(number_of_tweets_in_this_cycle))
+            logging.debug('%d tweets collected', number_of_tweets_in_this_cycle)
 
             # proceed to the next cycle
-            logging.debug('wait {} seconds before fetching the next result'.format(self._sleep_interval))
+            logging.debug('wait %d seconds before fetching the next result', self._sleep_interval)
             time.sleep(self._sleep_interval)
-            self._GetSearch()
+            self._get_search()
 
 
 class JST(tzinfo):
@@ -298,11 +316,20 @@ def utc_to_jst(utc_dt):
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config['Log'] = {}
-    config['Twitter'] = {'most_recent_id': '0', 'base_query': DEFAULT_QUERY, 'data_file': DEFAULT_DATA_FILE, 'sleep_interval': DEFAULT_SLEEP_INTERVAL, 'current_month': get_current_month()}
+    config['Twitter'] = {
+            'most_recent_id': '0',
+            'base_query': DEFAULT_QUERY,
+            'data_file': DEFAULT_DATA_FILE,
+            'sleep_interval': DEFAULT_SLEEP_INTERVAL,
+            'current_month': get_current_month(),
+            'log_rotate': True}
     config.read(CONFIG_FILE)
 
     if 'filename' in config['Log']:
-        logging.basicConfig(filename=config['Log']['filename'], format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
+        logging.basicConfig(
+                filename=config['Log']['filename'],
+                format='%(asctime)s %(levelname)s:%(message)s',
+                level=logging.INFO)
     else:
         logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -316,8 +343,8 @@ if __name__ == "__main__":
             data_file=config['Twitter']['data_file'],
             sleep_interval=int(config['Twitter']['sleep_interval']),
             current_month=config['Twitter']['current_month'])
-    except KeyError as e:
-        print("Error: {} parameter was not defined in {}".format(e, CONFIG_FILE))
+    except KeyError as key_error:
+        print("Error: {} parameter was not defined in {}".format(key_error, CONFIG_FILE))
         sys.exit(1)
 
-    collector.RunForever()
+    collector.run_forever()
